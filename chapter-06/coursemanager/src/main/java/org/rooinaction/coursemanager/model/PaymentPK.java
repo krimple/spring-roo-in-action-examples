93b0297944269047eff73d1a48df09c54a4d7344
Ken Rimple <krimple@chariotsolutions.com>
2011-11-27 20:51:59 -0500

    removing stuff

--- chapter-06/coursemanager/src/main/java/org/rooinaction/coursemanager/model/PaymentPK.java
+++
@@ -1,9 +0,0 @@
-package org.rooinaction.coursemanager.model;
-
-import org.springframework.roo.addon.entity.RooIdentifier;
-import org.springframework.roo.addon.serializable.RooSerializable;
-
-@RooIdentifier(dbManaged = true)
-@RooSerializable
-public final class PaymentPK {
-}
