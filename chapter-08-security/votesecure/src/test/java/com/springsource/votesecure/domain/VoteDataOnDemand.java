b5da0511cc314a0af99545c1968cefb5607adf97
srinip2007 <srinipenchikala@gmail.com>
2012-04-08 17:09:50 -0500

    Deleted the old sample applications that are no longer referenced in the book.

--- chapter-08-security/votesecure/src/test/java/com/springsource/votesecure/domain/VoteDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package com.springsource.votesecure.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import com.springsource.votesecure.domain.Vote;
-
-@RooDataOnDemand(entity = Vote.class)
-public class VoteDataOnDemand {
-}
