453899482fd6c6f209b0f37457463785c00debf8
Ken Rimple <krimple@chariotsolutions.com>
2012-04-08 16:16:17 -0400

    removed old chapter 8 samples

--- chapter-8/cmsapp/src/test/java/com/rooinaction/cmsapp/domain/CourseDetailsDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.rooinaction.cmsapp.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = CourseDetails.class)
-public class CourseDetailsDataOnDemand {
-}
