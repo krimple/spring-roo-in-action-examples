dc74dca212e758b9713f6f40c6ea652a5400de7b
Ken Rimple <krimple@chariotsolutions.com>
2012-04-08 16:09:07 -0400

    removed old junk in testing dir

--- chapter-09-testing/votesecure/src/test/java/com/springsource/votesecure/domain/ChoiceDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package com.springsource.votesecure.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import com.springsource.votesecure.domain.Choice;
-
-@RooDataOnDemand(entity = Choice.class)
-public class ChoiceDataOnDemand {
-}
