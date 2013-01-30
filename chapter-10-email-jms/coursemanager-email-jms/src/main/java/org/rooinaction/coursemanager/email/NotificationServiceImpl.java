c9bc595e80458a147672923262850d85c0a2875e
Srini Penchikala <srinipenchikala@gmail.com>
2013-01-30 11:58:14 -0600

    Spring Roo 1.2.3 related changes.

--- chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/email/NotificationServiceImpl.java
+++ chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/email/NotificationServiceImpl.java
@@ -2,26 +2,27 @@ package org.rooinaction.coursemanager.email;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.mail.MailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationServiceImpl implements NotificationService {
-	
+
 	private static final Log log = LogFactory.getLog(NotificationServiceImpl.class);
 
     @Autowired
     private transient MailSender mailTemplate;
 
     @Autowired
-    private transient SimpleMailMessage simpleMailMessage;
+    private transient SimpleMailMessage templateMessage;
 
     public void sendMessage(String mailTo, String message) {
-    	log.debug("Ready to send the email message.");
-        simpleMailMessage.setTo(mailTo);
-        simpleMailMessage.setText(message);
-        mailTemplate.send(simpleMailMessage);
+		log.debug("Ready to send the email message.");
+
+        templateMessage.setTo(mailTo);
+        templateMessage.setText(message);
+        mailTemplate.send(templateMessage);
     }
 }
