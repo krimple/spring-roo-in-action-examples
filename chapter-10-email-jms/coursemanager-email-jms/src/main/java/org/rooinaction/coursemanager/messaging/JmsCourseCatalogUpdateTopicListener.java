f87ce9c7f33252e9eafbdd0cdd10de50e2a04eda
Srini Penchikala <srinipenchikala@gmail.com>
2013-01-30 11:58:50 -0600

    Spring Roo 1.2.3 related changes.

--- chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/messaging/JmsCourseCatalogUpdateTopicListener.java
+++ chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/messaging/JmsCourseCatalogUpdateTopicListener.java
@@ -1,13 +1,9 @@
 package org.rooinaction.coursemanager.messaging;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class JmsCourseCatalogUpdateTopicListener {
-	
-	private static final Log log = LogFactory.getLog(JmsCourseCatalogUpdateTopicListener.class);
 
     public void onMessage(Object message) {
-        log.debug("JMS message received: " + message);
+        System.out.println("JMS message received: " + message);
     }
 }
--- chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/messaging/JmsCourseRegistrationWaitListQueueListener.java
+++ chapter-10-email-jms/coursemanager-email-jms/src/main/java/org/rooinaction/coursemanager/messaging/JmsCourseRegistrationWaitListQueueListener.java
@@ -1,13 +1,9 @@
 package org.rooinaction.coursemanager.messaging;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class JmsCourseRegistrationWaitListQueueListener {
 
-	private static final Log log = LogFactory.getLog(JmsCourseRegistrationWaitListQueueListener.class);
-
     public void onMessage(Object message) {
-        log.debug("JMS message received: " + message);
+        System.out.println("JMS message received: " + message);
     }
 }
